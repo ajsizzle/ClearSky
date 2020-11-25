00d577a75f5c8f830c7b2c65fbedf69b9fb22194
Alex Johnson <ajohnson2389@gmail.com>
2020-11-25 17:34:21 -0500

    Initial Commit

---
+++ Clear Sky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clear Sky
+//
+//  Created by Alex Johnson on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
