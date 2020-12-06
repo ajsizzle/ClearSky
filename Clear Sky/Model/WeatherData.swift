4ced5dde6cead0cbea99a162f9a506b0e867d352
Alex Johnson <ajohnson2389@gmail.com>
2020-12-06 14:37:04 -0500

    Used JSON Decoder to successfully parse the data coming from OpenWeather API

---
+++ Clear Sky/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Clear Sky
+//
+//  Created by Alex Johnson on 12/2/20.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
---
+++ Clear Sky/Model/WeatherManager.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherManager.swift
+//  Clear Sky
+//
+//  Created by Alex Johnson on 12/1/20.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=66701032faba5b85ff64ad5e075d6a38"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+                
+            //3. Give the session a task
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+                
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
+    
+}
